60907d573c0ad9e4bc9eaa0f839c12df3f80cd05
Angel C Ramirez <angelcramirez@users.noreply.github.com>
2020-07-17 13:22:45 -0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+jQuery(function ($) {
+
+    'use strict';
+
+    // --------------------------------------------------------------------
+    // PreLoader
+    // --------------------------------------------------------------------
+
+    (function () {
+        $('#preloader').delay(200).fadeOut('slow');
+    }());
+
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+      });
+
+
+
+}); // JQuery end
