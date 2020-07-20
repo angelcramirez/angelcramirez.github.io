94924fa773f9fe5e35c85a4ce23921ee32b8ecce
Angel C Ramirez <angelcramirez@users.noreply.github.com>
2020-07-20 04:46:43 -0500

    added fonts, and fixed contact form

---
+++ js/grecaptcha.js
@@ -0,0 +1,48 @@
+// Global variables. Initialization
+urlRedirectJS = null; 
+
+$(function() {
+    function urlRedirect() {
+        $(document).ready(function(){
+            $("#ajaxForm").submit(function(e){
+                e.preventDefault();
+                var href = $(this).attr("action");
+                $.ajax({
+                    type: "POST",
+                    dataType: "json",
+                    url: href,
+                    data: $(this).serialize(),
+                    success: function(response){
+                        if(response.status == "success"){
+                            window.location.href = 'https://angelcramirez.github.io/thank-you.html'; // change this. 
+                        }else{
+                            alert("An error occured: " + response.message);
+                        }
+                    }
+                });
+            });
+        });
+    }
+    
+    urlRedirectJS = urlRedirect;
+
+});
+
+
+// reCAPTCHA Script. Token will not expire until you interact with form
+grecaptcha.ready(function() {
+    document.getElementById('ajaxForm').addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    grecaptcha.execute('6Lcro7IZAAAAAG934YEUBJBAKIMKAHuaXy7Dj0xY', {action: 'homepage'}).then(function(token) {
+        document.getElementById('captchaResponse').value = token;
+        urlRedirectJS();
+        document.getElementById('submit-btn').click();
+    });        
+    }, false);
+});
+
+
+
+
--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,34 @@ jQuery(function ($) {
         $('[data-toggle="tooltip"]').tooltip()
       });
 
+    // --------------------------------------------------------------------
+    // Contact Form
+    // --------------------------------------------------------------------
+    
+    // Disable Submit button until all required fields are filled 
+    $(function (){
+        $('#submit-btn').attr('disabled', true);
+    });
 
+    var fields = "#InputName, #InputEmail, #message-text";
+
+    $(fields).keyup(function() {
+        if (allFilled()) {
+            $('#submit-btn').removeAttr('disabled');
+    } else {
+        $('#submit-btn').attr('disabled', 'disabled');
+    }
+    });
+
+    function allFilled() {
+        var filled = true;
+        $(fields).each(function() {
+            if ($(this).val() == '') {
+                filled = false;
+            }
+        });
+        return filled;
+    }
+   
 
 }); // JQuery end
