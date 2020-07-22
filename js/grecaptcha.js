e334689a9c54512c9f644d929338a33cbf6eea76
Angel C Ramirez <44124616+angelcramirez@users.noreply.github.com>
2020-07-21 22:15:36 -0500

    Update grecaptcha.js

--- js/grecaptcha.js
+++ js/grecaptcha.js
@@ -38,8 +38,9 @@ grecaptcha.ready(function() {
 
     grecaptcha.execute('6Lcro7IZAAAAAG934YEUBJBAKIMKAHuaXy7Dj0xY', {action: 'homepage'}).then(function(token) {
         document.getElementById('captchaResponse').value = token;
-        document.getElementById('submit-btn').click();
-        document.getElementById('submit-btn').setAttribute('disabled', 'disabled');
+        const submitButton = document.getElementById('submit-btn');
+        submitButton.click();
+        submitButton.disabled = true;
         urlRedirectJS();
     });        
     }, false);
