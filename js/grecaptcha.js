6ab6c46face76358585ea1543bbef4814dda3f82
Angel C Ramirez <angelcramirez@users.noreply.github.com>
2020-07-22 03:57:51 -0500

    changed form validations

--- js/grecaptcha.js
+++ js/grecaptcha.js
@@ -32,13 +32,14 @@ $(function() {
 
 // reCAPTCHA Script. Token will not expire until you interact with form
 grecaptcha.ready(function() {
-      document.getElementById('ajaxForm').addEventListener("submit", function(event) {
+    document.getElementById('ajaxForm').addEventListener("submit", function(event) {
         event.preventDefault();
 
         grecaptcha.execute('6Lcro7IZAAAAAG934YEUBJBAKIMKAHuaXy7Dj0xY', {action: 'homepage'}).then(function(token) {
            document.getElementById('captchaResponse').value = token; 
            submitButton.click();
            urlRedirectJS();
+           
            submitButton.disabled = true;
         });        
       }, false);
