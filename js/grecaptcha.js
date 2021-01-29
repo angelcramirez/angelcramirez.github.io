f18c82cadea8467cc9ad12331bfef9f906af848b
Angel C Ramirez <44124616+angelcramirez@users.noreply.github.com>
2021-01-29 17:49:18 -0600

    Update grecaptcha.js

--- js/grecaptcha.js
+++ js/grecaptcha.js
@@ -36,11 +36,10 @@ $(function() {
                 event.preventDefault();
                   grecaptcha.execute('6Lcro7IZAAAAAG934YEUBJBAKIMKAHuaXy7Dj0xY', {action: 'homepage'}).then(function(token) {
                        document.getElementById('captchaResponse').value = token;
+                       urlRedirectJS();
+                       submitButton.click();
                    });        
             }, false);
-            
-                urlRedirectJS();
-                submitButton.click();
              }
         });
 
