cdb4c648015bb8ec89530f34fd5aab7a79407151
Angel C Ramirez <44124616+angelcramirez@users.noreply.github.com>
2021-01-28 19:35:59 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -15,35 +15,4 @@ jQuery(function ($) {
       });
 
 
-    // --------------------------------------------------------------------
-    // Contact Form
-    // --------------------------------------------------------------------
-    
-    // Disable Submit button until all required fields are filled 
-    $(function (){
-        $('#submit-btn').attr('disabled', true);
-    });
-
-    var fields = "#InputName, #InputEmail, #message-text";
-
-    $(fields).keyup(function() {
-        if (allFilled()) {
-            $('#submit-btn').removeAttr('disabled');
-        } else {
-            $('#submit-btn').attr('disabled', 'disabled');
-        }
-    });
-
-    function allFilled() {
-        var filled = true;
-        $(fields).each(function() {
-            if ($(this).val() == '') {
-                filled = false;
-            }
-        });
-        return filled;
-    }
-
-   
-
 }); // JQuery end
