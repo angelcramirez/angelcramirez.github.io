a1b1b700a7fce63433c548df12c2b1b6a4bd8194
Angel C Ramirez <44124616+angelcramirez@users.noreply.github.com>
2021-01-29 23:45:11 -0600

    Update grecaptcha.js

--- js/grecaptcha.js
+++ js/grecaptcha.js
@@ -41,11 +41,12 @@ grecaptcha.ready(function() {
 
         grecaptcha.execute('6Lcro7IZAAAAAG934YEUBJBAKIMKAHuaXy7Dj0xY', {action: 'homepage'}).then(function(token) {
            document.getElementById('captchaResponse').value = token;
-            urlRedirect();
-            
+
             if(submitButton.disabled && buttonClicked) return false; {
             }else {
-                console.log("Aqui toy");
+                urlRedirectJS();
+                submitButton.click();
+                submitButton.disabled = true;
             }
         });        
       }, false);
