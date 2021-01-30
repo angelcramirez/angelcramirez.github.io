c32e8b2f900b503379e64b2be41b89c6a89ca33b
Angel C Ramirez <44124616+angelcramirez@users.noreply.github.com>
2021-01-29 18:43:26 -0600

    Update grecaptcha.js

--- js/grecaptcha.js
+++ js/grecaptcha.js
@@ -37,9 +37,7 @@ grecaptcha.ready(function() {
 
         grecaptcha.execute('6Lcro7IZAAAAAG934YEUBJBAKIMKAHuaXy7Dj0xY', {action: 'homepage'}).then(function(token) {
            document.getElementById('captchaResponse').value = token; 
-           urlRedirectJS();
-           submitButton.disabled = true;
-            
+           urlRedirectJS(); 
         });        
       }, false);
 
