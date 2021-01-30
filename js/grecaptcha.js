4063576f67e102247224f11c87b1037d051305e0
Angel C Ramirez <44124616+angelcramirez@users.noreply.github.com>
2021-01-30 11:05:18 -0600

    Update grecaptcha.js

--- js/grecaptcha.js
+++ js/grecaptcha.js
@@ -5,7 +5,7 @@ const submitButton = document.getElementById('submit-btn');
 $(function() {
     function urlRedirect() {
         $(function(){
-            if($("#submit-btn").attr('disabled')) return false;
+            if($("#submit-btn").attr('disabled')) console.log('The button is disabled');
             else {
                 $("#ajaxForm").submit(function(e){
                 e.preventDefault();
