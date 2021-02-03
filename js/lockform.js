a7d2e4965aa01be7173697fd46ea1743d0ceb531
Angel C Ramirez <44124616+angelcramirez@users.noreply.github.com>
2021-02-03 16:02:18 -0600

    Update lockform.js

--- js/lockform.js
+++ js/lockform.js
@@ -2,25 +2,25 @@ $(document).ready(function() {
   const lockModal = $("#lock-modal");
   const loadingCircle = $("#loading-circle");
   const form = $("#ajaxForm");
-  var name = document.forms[0]["name"];
-  var message = document.forms[0]["message"];
-  var email = document.forms[0]["email"];
-  var subject = document.forms[0]["subject"];
   
-  if(name = "" && message = "" && email = "" && subject = "") console.log(name);
-  else{
-
   form.on('submit', function(e) {
     e.preventDefault(); //prevent form from submitting
     
+    var formEmpty = false;
+
+   $(this).find('input[type!="hidden"]').each(function () {
+      if ( ! $(this).val() ) { has_empty = true; return false; }
+   });
+
+   if ( formEmpty ) return false; 
+   else{ 
     lockModal.css("display", "block");
     loadingCircle.css("display", "block");
 
     form.children("input").each(function() {
       $(this).attr("readonly", true);
     });
+   }
   });
-    
-  }
   
 });
